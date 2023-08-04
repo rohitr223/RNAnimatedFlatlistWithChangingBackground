db4f4977a6c9ea72185f398f2dd9188fe9a111ef
rohit <rohitran236@gmail.com>
2023-08-04 12:04:09 +0530

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,156 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef } from "react";
+import {
+  Animated,
+  SafeAreaView,
+  StyleSheet,
+  Dimensions,
+  Image,
+  View,
+} from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { DATA } from "./data";
 
-export default function App() {
+const App = () => {
+  // get the window dimensions
+  const width = Dimensions.get("window").width;
+  const height = Dimensions.get("window").height;
+
+  // adjusting the screen height & width according to our requirements
+  const screenWidth = width * 0.7;
+  const screenHeight = height * 0.5;
+
+  // inititlize scroll animation value
+  const scrollX = useRef(new Animated.Value(0)).current;
+
+  // background image height
+  const backDrop = height * 0.5;
+
+  // Function for loading background image
+  const BackDropImage = ({ scrollX }) => {
+    return (
+      <View
+        style={
+          ([
+            {
+              height: backDrop,
+              width,
+              position: "absolute",
+              top: 0,
+            },
+          ],
+          StyleSheet.absoluteFillObject)
+        }
+      >
+        {DATA.map((item, index) => {
+          //console.log(index);
+          const inputRange = [
+            (index - 1) * screenWidth,
+            index * screenWidth,
+            (index + 1) * screenWidth,
+          ];
+
+          const outputRange = [0, 1, 0];
+
+          const opacity = scrollX.interpolate({
+            inputRange,
+            outputRange,
+          });
+          return (
+            <Animated.Image
+              source={{ uri: item.image }}
+              key={index}
+              style={{
+                height: backDrop,
+                width: "100%",
+                position: "absolute",
+                top: 0,
+                opacity,
+              }}
+            />
+          );
+        })}
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar color="#fff" />
+      <BackDropImage scrollX={scrollX} />
+      <Animated.FlatList
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        bounces={false}
+        decelerationRate={0}
+        snapToInterval={screenWidth}
+        scrollEventThrottle={16}
+        onScroll={Animated.event(
+          [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+          { useNativeDriver: true }
+        )}
+        data={DATA}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item, index }) => {
+          // using animation for Flatlist
+          const inputRange = [
+            (index - 1) * screenWidth,
+            index * screenWidth,
+            (index + 1) * screenWidth,
+          ];
+
+          const outputRange = [0, -50, 0];
+
+          const translateY = scrollX.interpolate({
+            inputRange,
+            outputRange,
+          });
+
+          return (
+            <Animated.View
+              style={[
+                styles.cardContainer,
+                {
+                  backgroundColor: item.color,
+                  width: screenWidth,
+                  height: screenHeight,
+                  transform: [{ translateY }],
+                },
+              ]}
+            >
+              <Image source={{ uri: item.image }} style={styles.bgImg} />
+            </Animated.View>
+          );
+        }}
+        contentContainerStyle={styles.flatList}
+      />
+    </SafeAreaView>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#252525",
+  },
+  flatList: {
+    paddingTop: "70%",
+    alignItems: "center",
+  },
+  cardContainer: {
+    borderWidth: 10,
+    borderColor: "#EFEFEF",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 20,
+    margin: 10,
+  },
+  bgImg: {
+    height: "100%",
+    width: "100%",
+    borderRadius: 20,
   },
 });
---
+++ data.js
@@ -0,0 +1,42 @@
+export const DATA = [
+    {
+        id: 1,
+        image: 'https://images.unsplash.com/photo-1514525253161-7a46d19cd819?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8Y29uY2VydHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+        id: 2,
+        image: 'https://images.unsplash.com/photo-1563841930606-67e2bce48b78?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8Y29uY2VydHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+        id: 3,
+        image: 'https://images.unsplash.com/photo-1567942712661-82b9b407abbf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fGNvbmNlcnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+        id: 4,
+        image: 'https://images.unsplash.com/photo-1583795484071-3c453e3a7c71?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fGNvbmNlcnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+        id: 5,
+        image: 'https://images.unsplash.com/photo-1501612780327-45045538702b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fGNvbmNlcnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+        id: 6,
+        image: 'https://images.unsplash.com/photo-1507676184212-d03ab07a01bf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjR8fGNvbmNlcnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+        id: 7,
+        image: 'https://images.unsplash.com/photo-1501281668745-f7f57925c3b4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjd8fGNvbmNlcnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+        id: 8,
+        image: 'https://images.unsplash.com/photo-1506157786151-b8491531f063?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fGNvbmNlcnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+        id: 9,
+        image: 'https://images.unsplash.com/photo-1470225620780-dba8ba36b745?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzV8fGNvbmNlcnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60',
+    },
+    {
+        id: 10,
+        image: 'https://images.unsplash.com/photo-1516450360452-9312f5e86fc7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDF8fGNvbmNlcnR8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60',
+    },
+];
